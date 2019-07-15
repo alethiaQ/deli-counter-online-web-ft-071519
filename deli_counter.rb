1b49b75394139af5f9acb14740659dc656d25286
Alethia Quintero <alethiaq22@gmail.com>
2019-07-15 20:59:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,13 +15,5 @@ def line(arr)
 end
 
 def take_a_number(arr, name)
-  if arr.length == 0
-    arr << name
-    puts "Welcome, #{name}. You are number 1 in line"
-  else
-    arr.each_with_index do |per, index|
-      arr << name
-      puts "Welcome, #{per}. You are number #{index +1} in line" unless name != per
-    end
-  end
+
 end
